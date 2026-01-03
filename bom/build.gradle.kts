6d383620605508582a0aa2028a1a5f7d22a15c2e
Mohammad ali Riazati <farsroidx@gmail.com>
2026-01-03 03:52:22 +0330

    feat: Bom

---
+++ bom/build.gradle.kts
@@ -0,0 +1,60 @@
+import kotlin.text.replace
+
+plugins {
+
+    // github
+    alias(libs.plugins.java.platform)
+
+    // maven
+    alias(libs.plugins.maven.publish)
+
+}
+
+javaPlatform { allowDependencies() }
+
+dependencies {
+
+    constraints {
+
+        val reason = "Andromeda modules must be consumed via the BOM to ensure version alignment."
+
+        rootProject.subprojects
+            .filter { project ->
+                project.name.startsWith("lib-")
+            }
+            .forEach { module ->
+
+                module.afterEvaluate {
+
+                    val artifactId =
+                        module.name.replace("lib-", "andromeda-")
+
+                    val coordinate =
+                        "${group}:$artifactId:${module.version}"
+
+                    println("✅ BOM Bind: $coordinate")
+
+                    api(coordinate) { because(reason) }
+                }
+            }
+    }
+}
+
+afterEvaluate {
+
+    publishing {
+
+        publications {
+
+            register<MavenPublication>("maven") {
+
+                groupId = group.toString()
+                version = version.toString()
+
+                artifactId = "andromeda-bom"
+
+                from(components["javaPlatform"])
+            }
+        }
+    }
+}
